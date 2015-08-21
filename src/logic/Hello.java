614d761fa1d6d899a16d089c9adfd6a2dd0efc94
Ascharapon Sinlamuth <greatlife224@gmail.com>
2015-08-21 11:23:23 +0700

    hello

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,11 +15,23 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "Hello GitHub";
 	}
 	
 	public static int[] bubbleSort(int[] array){
 		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
+			
+		for (int i=0; i<array.length -1; i++) {
+			for (int g=0; g<array.length-1; g++) {
+				if (array[g] > array[g+1]){
+					int t = array[g];
+					array[g] = array[g+1];
+					array[g+1]=t;
+					
+				}
+				
+			}
+		}
 		return array;
 	}
 	
